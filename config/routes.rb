1bc16ba70772b3bf6aa72022f7fff233aa784c38
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-11-09 17:53:49 +0200

    show done

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 # 1. As a user, I can list tasks
-get '/tasks', to: 'tasks#index'
+get '/tasks', to: 'tasks#index', as: :tasks
 # 2. As a user, I can view the details of a task
-get '/tasks/:id', to: 'tasks#show'
+get '/tasks/:id', to: 'tasks#show', as: :task
 # 3. As a user, I can add a new task
 get '/tasks/new', to: 'tasks#new' # display the form
 post '/tasks/', to: 'tasks#create'
